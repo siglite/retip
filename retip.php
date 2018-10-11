b9b48318356b62ee8e001f2417c7930c52aef82e
Shun Ishihara <sig@siglite.net>
2018-10-11 19:15:21 +0900

    [WIP]

---
+++ retip.php
@@ -0,0 +1,16 @@
+<?php
+// Redirect if access by browser
+$ua = $_SERVER['HTTP_USER_AGENT'];
+if (preg_match('/^Mozilla\/5.0/', $ua) === 1) {
+    echo "hoge";
+    exit(1)
+}
+
+// Return IP address
+header('Content-Type: application/json');
+$json = [
+    'ip' => $_SERVER['REMOTE_ADDR']
+];
+echo json_encode($json, JSON_PRETTY_PRINT)
+
+?>
